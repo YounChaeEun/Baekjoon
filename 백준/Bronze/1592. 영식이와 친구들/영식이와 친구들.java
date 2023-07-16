ac1a013547699544914f95dc8b409074c6c65621
YounChaeEun <111855256+YounChaeEun@users.noreply.github.com>
2023-07-17 01:18:10 +0900

    Update 영식이와 친구들.java

--- 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -11,28 +11,28 @@ public class Main {
 		int count = 0;
 		seat[1] = 1;
 		int index = 1;
-		
-		while(true) {
-			if(seat[index] == m) {
-				break;
-			}
-			else if(seat[index] % 2 == 1) {
-				index += l;
-				if(index > n) {
-					index -= n;
-				}
-				seat[index] += 1;
-				count += 1;
-			}
-			else if(seat[index] % 2 == 0) {
-				index -= l;
-				if (index <= 0) {
-					index += n;
-				}
-				seat[index] += 1;
-				count += 1;
-			}
+
+		while (seat[index] != m) {
+	    		if (seat[index] % 2 == 1) {
+	    			index = moveIndex(l, index, n);
+	    		} else if (seat[index] % 2 == 0) {
+	    			index = moveIndex(-l, index, n);
+	    		}
+	    		seat[index]++;
+	    		count++;
+	    	}
+	    
+	    	scanner.close();
+	    	System.out.println(count);
+	}
+	
+	private static int moveIndex(int step, int currentIndex, int length) {
+		int nextIndex = currentIndex + step;
+		if (nextIndex > length) {
+			nextIndex -= length;
+		} else if (nextIndex <= 0) {
+			nextIndex += length;
 		}
-		System.out.println(count);
+		return nextIndex;
 	}
 }
