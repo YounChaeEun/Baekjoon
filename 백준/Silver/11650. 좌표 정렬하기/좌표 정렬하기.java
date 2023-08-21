4aa7c3a04c983e17024441831a4da3c4e4243c78
YounChaeEun <111855256+YounChaeEun@users.noreply.github.com>
2023-08-22 01:38:31 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1508 ms, Memory: 57068 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,16 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
 		int coord[][] = new int[t][2];
 		
+		
 		for(int i=0; i<t; i++) {
-			coord[i][0] = sc.nextInt();
-			coord[i][1] = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			coord[i][0] = Integer.parseInt(st.nextToken());
+			coord[i][1] = Integer.parseInt(st.nextToken());
 			
 		}
 		Arrays.sort(coord, (o1, o2) -> {
