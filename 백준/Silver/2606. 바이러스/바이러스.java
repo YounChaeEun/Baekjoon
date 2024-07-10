6201db22b9b77d590f96d95ba98459b315941cd5
Chaeeun Youn <amy4021186@gmail.com>
2024-07-10 17:03:34 +0900

    [Silver III] Title: 바이러스, Time: 204 ms, Memory: 17928 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class Main {
+	static int a;
+	static int b;
+	static boolean[] visited;
+	static int[][] graph;
+	static int count = 0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		visited = new boolean[a+1];
+		graph = new int[a+1][a+1];
+
+		for(int i=0; i<b; i++) {
+			int j = sc.nextInt();
+			int k = sc.nextInt();
+			graph[j][k]=1;
+			graph[k][j]=1;	
+		}
+		bfs(1);
+		System.out.println(count);
+		sc.close();
+	}
+	public static void bfs(int startNode) {
+		visited[startNode] = true;
+		for(int i=0; i<a+1; i++) {
+			if(!visited[i] && graph[startNode][i]==1) {
+				bfs(i);
+				count++;
+			}
+		}
+	}
+}
